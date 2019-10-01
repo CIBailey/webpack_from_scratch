f261fece6011f31668170807fe2912fc9025506d
Christina <christinaibailey@live.com>
2019-10-01 15:31:16 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 const webpack = require('webpack');
+const path = require('path');
 
 //define starting point
 let config = {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
+        path: path.resolve(__dirname, './public'),
         filename: 'output.js'    //output is what we link to our entry point html file in order to "output" to our html file
     },
     mode: 'none',
@@ -12,6 +14,10 @@ let config = {
             test: /\.js$/, // files ending with .js
             exclude: /node_modules/, // exclude the node_modules directory
             loader: "babel-loader" // use this (babel-core) loader
+        },
+        {
+            test: /\.scss$/, // files ending with .js
+            loader: ['style-loader', 'css-loader', 'sass-loader'] // use this (babel-core) loader
         }]
     }
 }
