b43b7920816d776b9c26c6092e9c52f7240deb46
Christina <christinaibailey@live.com>
2019-10-04 10:59:23 +0200

    finish part one

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,34 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextWebpackPlugin = require("extract-text-webpack-plugin");
 
 //define starting point
 let config = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, './public'),
+        path: path.resolve(__dirname, './public'), //path
         filename: 'output.js'    //output is what we link to our entry point html file in order to "output" to our html file
     },
     mode: 'none',
     module: {
-        rules: [{
+        rules: [
+            {
             test: /\.js$/, // files ending with .js
             exclude: /node_modules/, // exclude the node_modules directory
             loader: "babel-loader" // use this (babel-core) loader
         },
         {
-            test: /\.scss$/, // files ending with .js
-            loader: ['style-loader', 'css-loader', 'sass-loader'] // use this (babel-core) loader
-        }]
-    }
+            test: /\.scss$/, // files ending with .scss,
+            use: ExtractTextWebpackPlugin.extract({ //call our plugin with extract method
+                use:['css-loader','sass-loader'], // use these loaders
+                fallback: 'style-loader' //fallback for any css not extracted
+            })
+        },
+    ]
+    },
+    plugins: [
+        new ExtractTextWebpackPlugin('styles.css')
+    ]
 }
 
 //then export the moduel
